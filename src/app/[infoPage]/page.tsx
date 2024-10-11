c505dadb83e034d04862ddfad3bdc569f890d21c
nolipot <dfabs333@gmail.com>
2024-10-11 16:27:34 -0400

    Testing env vars

--- src/app/[infoPage]/page.tsx
+++ src/app/[infoPage]/page.tsx
@@ -9,7 +9,7 @@ export default function InfoPage( { params } : {
 } ) {
     const linkMap: any = { professionalism: <Professionalism/>, academics: <Academics/>, networking: <Networking/>, events: <Events/> };
     const linkPage: any = params.infoPage;
-    console.log(linkPage,  params, linkMap);
+    console.log(linkPage,  Object.keys(params), linkMap);
     return (
         <>
             <span>{linkPage.toString()}</span>
