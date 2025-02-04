8d5f5384204c97ea1081b8946bf46218533e28c2
Tatiana <tatiana.joli@outlook.com>
2025-02-03 19:35:54 -0500

    Added shpetinas folder for images and events component. Edited events component for testing.

--- src/app/shpetinas/page.tsx
+++ src/app/shpetinas/page.tsx
@@ -1,9 +1,12 @@
 import ResourceComponent from '@/components/resources-shpetinas.component';
+import EventsComponent from '@/components/events-shpetinas.component';
 
 export default function Shpetinas () {
     return (
         <>
             <h1>TESTING</h1>
+            
+            <EventsComponent />
             <ResourceComponent />
         </>
     );
---
+++ src/components/events-shpetinas.component.tsx
@@ -0,0 +1,9 @@
+export default function ShpetinasEvents () {
+    return (
+        <>
+            <div className="flex flex-row bg-main text-white w-1/2 rounded-md p-5 gap-5 mx-auto my-5">
+                <h1>test event</h1>
+            </div>
+        </>
+    );
+}
