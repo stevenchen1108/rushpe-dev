dc14a74e6ee02b5864b597757103d2857c5e5b48
Tatiana <tatiana.joli@outlook.com>
2025-02-18 23:08:37 -0500

    changes to sizing when viewing from different sized screens

--- src/components/events-shpetinas.component.tsx
+++ src/components/events-shpetinas.component.tsx
@@ -5,10 +5,10 @@ export default function ShpetinasEvents() {
     return (
         <>
             <div className="flex flex-col bg-[#ffdbf8] text-black p-4 mx-auto">
-                <div className="flex flex-col bg-white rounded-3xl p-5 mb-5">
+                <div className="flex flex-col bg-white rounded-3xl p-5 mb-5 lg:mx-80 md:mx-8 sm:mx-0">
                     <h1 className="text-center font-bold tracking-wide text-5xl mx-auto my-auto">UPCOMING EVENTS</h1>
                 </div>
-                <ul className="flex flex-col gap-6 mx-8">
+                <ul className="flex flex-col gap-6 xl:mx-56 lg:mx-48 md:mx-16 sm:mx-8">
                     <li className="bg-white rounded-md shadow-md p-6">
                         <div className="flex flex-col w-full text-center items-center tracking-wide">
                             <div className="flex justify-center">
@@ -16,13 +16,13 @@ export default function ShpetinasEvents() {
                             </div>
                             <div className="text-center">
                                 <h2 className="mt-5 mb-3 font-bold text-[#841f70] text-3xl">Women's Day at Douglass</h2>
-                                <div className="text-xl">                                    
+                                <div className="text-3xl">                                    
                                     <p><strong>Date:</strong> March 6th</p>
                                     <p><strong>Location:</strong> Kathleen W. Ludwig Global Village Living Learning (Jameson Complex)</p>
                                     <p><strong>Time:</strong> 12pm - 3pm</p>
                                 </div>
-                                <div className="mt-5 text-lg">
-                                    <p> <strong>Celebrate international womanhood!</strong><br />
+                                <div className="mt-5 text-2xl">
+                                    <p> <strong>Celebrate International Womanhood!</strong><br />
                                         The annual Women’s Day at Douglass, presented by the Douglass Diversity, Equity, and Inclusion Program, 
                                         will highlight the accomplishments of Douglass students, alumnae, and women within the Rutgers community. 
                                         Women’s Day at Douglass will include messages from special 
@@ -36,17 +36,17 @@ export default function ShpetinasEvents() {
                     <li className="bg-white rounded-md shadow-md p-6">
                         <div className="flex flex-col w-full text-center items-center tracking-wide">
                             <div className="flex justify-center">
-                                <img className="rounded-2xl" src={EventImage2.src} alt="event-img-2" width={500} height={500} />
+                                <img className="rounded-3xl" src={EventImage2.src} alt="event-img-2" width={500} height={500} />
                             </div>
                             <div className="text-center">
                                 <h2 className="mt-5 mb-3 font-bold text-[#841f70] text-3xl">Spring Shadow Program</h2>
-                                <div className="text-xl">   
+                                <div className="text-2xl">   
                                     <p><strong>Dates: </strong> April 10th - 12th</p>
                                     <p><strong>Location: </strong> Busch Campus</p>
                                     <p><strong>Arrival Time:</strong> 5:30pm - 6:30pm (April 10)</p>     
                                     <p><strong>Departure Time:</strong> 3:30pm - 4:30pm (April 12)</p>                                 
                                 </div>
-                                <div className="mt-5 text-lg">
+                                <div className="mt-5 text-2xl">
                                     <p> This Shadow Program will be the host of professional companies and several student organizations that will 
                                         provide all the resources for the students that attend. <br/>
                                         The Shadow Program seeks young students in their <b>Sophomore, Junior, and Senior</b> years that are eager to grow as individuals, develop 
--- src/components/resources-shpetinas.component.tsx
+++ src/components/resources-shpetinas.component.tsx
@@ -6,29 +6,29 @@ export default function ShpetinasResources () {
     return (
         <>
             <div className="flex flex-col bg-[#ffdbf8] text-black p-4 mx-auto">
-                <div className="flex flex-col bg-white rounded-3xl p-5 mb-5">
+                <div className="flex flex-col bg-white rounded-3xl p-5 mb-5 lg:mx-80 md:mx-8 sm:mx-0">
                     <h1 className="text-center font-bold tracking-wide text-5xl mx-auto my-auto">RESOURCES</h1>
                 </div>
 
-                <div className="flex flex-col bg-white rounded-md text-black mb-8 mx-8">
+                <div className="flex flex-col bg-white rounded-md text-black mb-8 xl:mx-56 lg:mx-48 md:mx-16 sm:mx-8">
                     <ul className="flex flex-col list-none text-left shadow-md">
                         
-                        <li className="flex flex-col bg-[#ffa6a6] rounded-md mt-7" style={{minHeight: "300px"}}>
+                        <li className="flex flex-col bg-[#ffa6a6] rounded-md mt-7 p-6" style={{minHeight: "300px"}}>
                             
-                            <div className="flex flex-col md:flex-row items-center justify-center px-8 py-6 w-full">
-                                <img className="rounded-2xl mx-auto sm:w-8 md:w-32 lg:w-56 md:mr-5 md:ml-0 mb-4 md:mb-0" src={ResourceImage1.src} alt="event-img-1"/>
+                            <div className="flex flex-col md:flex-row items-center justify-center px-8 py-6 w-full lg:space-x-4">
+                                <img className="rounded-2xl mx-auto sm:w-8 md:w-32 lg:w-56 md:mr-5 mb-4 md:mb-0" src={ResourceImage1.src} alt="event-img-1"/>
                                 <div className="text-center flex-grow max-w-[90%] md:max-w-[60%]">
-                                    <h2 className="font-bold text-black text-4xl">RICAP</h2>
-                                    <h1 className="text-xl font-semibold">Rutgers Immigrant Community Assistance Project</h1>
+                                    <h2 className="font-bold text-black text-5xl">RICAP</h2>
+                                    <h1 className="text-3xl font-semibold">Rutgers Immigrant Community Assistance Project</h1>
                                 </div>
                             </div>
 
-                            <div className="text-left text-lg px-3 mt-3 mb-3">
+                            <div className="text-left text-2xl mt-3 mb-3">
                                 <p> <a className="underline text-blue-700 font-bold" href="https://law.rutgers.edu/information-for/get-legal-help/rutgers-immigrant-community-assistance-project">
                                     RICAP</a> strives to foster a safe environment on campus and to remove barriers to student success by <strong>providing immigration legal services</strong>.
                                     They host community forums, informational Immigration presentations, and Know-Your-Rights training on all Rutgers campuses. <br/>
                                 </p>
-                                <p className="mt-3">If you're seeking an appointment or have general questions, please contact RICAP:</p>
+                                <p className="mt-3 mb-3">If you're seeking an appointment or have general questions, please contact RICAP:</p>
                                 <ul className="list-disc list-inside pl-4">
                                     <li><strong>Request an Appointment</strong> with Immigration Attorney Jason Hernandez by emailing 
                                         <a className="underline text-blue-700 font-bold ml-1" href="mailto:mary.hewey@rutgers.edu">Mary Hewey</a>, the RICAP Paralegal.
@@ -43,15 +43,15 @@ export default function ShpetinasResources () {
                         
                         <li className="flex flex-col bg-[#ffc7a6] rounded-md mt-7" style={{minHeight: "300px"}}>
                             
-                            <div className="flex flex-col md:flex-row items-center justify-center px-8 py-6 w-full">
-                                <img className="rounded-2xl mx-auto sm:w-8 md:w-32 lg:w-56 md:mr-5 md:ml-0 mb-4 md:mb-0" src={ResourceImage2.src} alt="event-img-2" width={150} height={150}/>
+                            <div className="flex flex-col md:flex-row items-center justify-center px-8 py-6 w-full lg:space-x-4">
+                                <img className="rounded-2xl mx-auto sm:w-8 md:w-32 lg:w-56 md:mr-5 mb-4 md:mb-0" src={ResourceImage2.src} alt="event-img-2"/>
                                 <div className="text-center flex-grow max-w-[90%] md:max-w-[60%]">
-                                    <h2 className="font-bold text-black text-4xl">HESAA</h2>
-                                    <h1 className="text-xl font-semibold">Higher Education Student Assistance Authority</h1>
+                                    <h2 className="font-bold text-black text-5xl">HESAA</h2>
+                                    <h1 className="text-3xl font-semibold">Higher Education Student Assistance Authority</h1>
                                 </div>
                             </div>
                             
-                            <div className="text-left text-lg px-4 mt-3 mb-3">
+                            <div className="text-left text-2xl px-4 mt-3 mb-3">
                                 <p> <a className="underline text-blue-700 font-bold" href="https://www.hesaa.org/Pages/aboutus.aspx">
                                     HESAA</a> is the only New Jersey state agency with the soles mission of providing students and families with <strong>financial and informational resources</strong> for students to pursue their education beyond high school.
                                 </p>
@@ -65,21 +65,21 @@ export default function ShpetinasResources () {
                         
                         <li className="flex flex-col bg-[#ffeca6] rounded-md mt-7 mb-7" style={{minHeight: "300px"}}>
                             
-                            <div className="flex flex-col md:flex-row items-center justify-center px-8 py-6 w-full">
-                                <img className="rounded-2xl mx-auto sm:w-8 md:w-32 lg:w-56 md:mr-5 md:ml-0 mb-4 md:mb-0" src={ResourceImage3.src} alt="event-img-3" width={150} height={150}/>
+                            <div className="flex flex-col md:flex-row items-center justify-center px-8 py-6 w-full lg:space-x-4">
+                                <img className="rounded-2xl mx-auto sm:w-8 md:w-32 lg:w-56 md:mr-5 mb-4 md:mb-0" src={ResourceImage3.src} alt="event-img-3"/>
                                 <div className="text-center flex-grow max-w-[90%] md:max-w-[60%]">
-                                    <h2 className="font-bold text-black text-4xl">OWH</h2>
-                                    <h1 className="text-xl font-semibold">Office on Women's Health</h1>
+                                    <h2 className="font-bold text-black text-5xl">OWH</h2>
+                                    <h1 className="text-3xl font-semibold">Office on Women's Health</h1>
                                 </div>
                             </div>
                             
-                            <div className="text-left text-lg px-4 mt-3 mb-3">
+                            <div className="text-left text-2xl px-4 mt-3 mb-3">
                                 <p> <a className="underline text-blue-700 font-bold" href="https://law.rutgers.edu/information-for/get-legal-help/rutgers-immigrant-community-assistance-project">
                                     OWH</a> coordinates women's health efforts and addresses critical women's health issues.
                                     They inform and advance policies, educate health care professionals and consumers, and support innovative programs. 
-                                    Their goal is for <strong>all women and girls to achieve the best possible health</strong>. <br/>
-                                    Learn more about them and the programs they provide at their website, linked <a className="underline text-blue-700 font-bold" href="https://womenshealth.gov/about-us/what-we-do">here</a>.
-                                </p>                                
+                                    Their goal is for <strong>all women and girls to achieve the best possible health</strong>.
+                                </p>
+                                <p className="mt-3">Learn more about them and the programs they provide at their website, linked <a className="underline text-blue-700 font-bold" href="https://womenshealth.gov/about-us/what-we-do">here</a>.</p>                           
                             </div>
 
                         </li> 
