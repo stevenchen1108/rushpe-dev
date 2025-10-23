cee6856ca24ece3f0ea8cc02fdaeaaf6a71729b1
stevenchen <chensteven900@gmail.com>
2025-10-23 00:08:52 -0400

    changed send message to just send

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -316,7 +316,7 @@ export default function ContactUs() {
                         Sending…
                       </>
                     ) : (
-                      "Send Message"
+                      "Send"
                     )}
                   </button>
                 </div>
